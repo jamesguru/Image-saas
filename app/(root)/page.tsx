cfe69ee386593f4bc06ddb09eaf60e673341aa2b
unknown <jameskagunga2022@gmail.com>
2024-02-19 15:31:47 -0800

    added sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    Home
+    <p>Home</p>
+    <UserButton afterSignOutUrl="/" />
    </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPlex = IBM_Plex_Sans({ 
+const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
-  weight:['400','500','600','700'],
-  variable:'--font-ibm-plex'
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
 });
 export const metadata: Metadata = {
   title: "Imaginify",
@@ -19,10 +19,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: "#624cf5" },
+      }}
+    >
       <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
-    </html>
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
